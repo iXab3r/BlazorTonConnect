4cf569180891573e78a1db01f91363bea2331960
Xab3r <mail.xaber@gmail.com>
2024-05-15 17:16:18 +0200

    Initial commit

---
+++ BlazorTonConnect.Playground.Web/Program.cs
@@ -0,0 +1,45 @@
+var builder = Host
+    .CreateDefaultBuilder()
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder
+            .ConfigureServices(ConfigureServices)
+            .Configure((context, applicationBuilder) => Configure(context.HostingEnvironment, applicationBuilder))
+            .UseStaticWebAssets();
+    });
+
+await builder.Build().RunAsync();
+return;
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddRazorPages();
+    services.AddServerSideBlazor(options =>
+    {
+        options.DetailedErrors = true;
+    });
+}
+
+void Configure(IWebHostEnvironment env, IApplicationBuilder app)
+{
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+    }
+    
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapBlazorHub();
+        endpoints.MapFallbackToPage("/_Host");
+    });
+}
---
+++ BlazorTonConnect/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: InternalsVisibleTo("BlazorTonConnect.Playground")]
+[assembly: InternalsVisibleTo("BlazorTonConnect.Playground.WebAssembly")]
+[assembly: InternalsVisibleTo("BlazorTonConnect.Playground.Web")]
